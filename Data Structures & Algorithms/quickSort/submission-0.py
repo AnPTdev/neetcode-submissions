bef6b54ad605c508bc1b2bb050c3aa275b6e2e60
Jason Pham <100202126+AnPTdev@users.noreply.github.com>
2026-05-17 22:20:45 +0700

    Add: quickSort - submission-0

---
+++ Data Structures & Algorithms/quickSort/submission-0.py
@@ -0,0 +1,22 @@
+class Solution:
+    def quickSort(self, pairs: List[Pair]) -> List[Pair]:
+        self.quickSortHelper(pairs, 0, len(pairs)-1)
+        return pairs
+
+    def quickSortHelper(self, pairs: List[Pair], s: int, e:int) -> None:
+        if e - s + 1 <= 1:
+            return
+
+        pivot = pairs[e]
+        left = s
+        for i in range(s, e):
+            if pairs[i].key < pivot.key:
+                tmp = pairs[left]
+                pairs[left] = pairs[i]
+                pairs[i] = tmp
+                left+=1
+        pairs[e] = pairs[left]
+        pairs[left] = pivot
+
+        self.quickSortHelper(pairs, s, left - 1)
+        self.quickSortHelper(pairs, left + 1, e)
